276691b962626f814392154c424a17c4793dbd1b
anonimnianonist <vanjamba0211@gmail.com>
2026-08-09 00:54:01 +0300

    Update .lua

--- .lua
+++ .lua
@@ -88,7 +88,6 @@ local FarmGroup = Tabs.Farm:AddLeftGroupbox("Autofarm", "play")
 FarmGroup:AddButton("Start Autofarm", function()
     _G.AutofarmEnabled = true
     Library:Notify("Autofarm started!", 2)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfx-qudsa/newauto/refs/heads/main/.lua"))()
 end)
 
 ThemeManager:SetLibrary(Library)
