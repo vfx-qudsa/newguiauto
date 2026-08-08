fd6e2ef826772b9131c23c22f8dfe266d9413651
anonimnianonist <vanjamba0211@gmail.com>
2026-08-09 02:07:12 +0300

    Update .lua

--- .lua
+++ .lua
@@ -1,23 +1,26 @@
-
 local RunService = game:GetService("RunService")
 local Stats = game:GetService("Stats")
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
 local CoreGui = game:GetService("CoreGui")
 local lp = Players.LocalPlayer
-
 if CoreGui:FindFirstChild("Crazy Diamond") then 
     return 
 end
+
+queue_on_teleport([[
+    _G.AutofarmRunning = nil
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfx-qudsa/newguiauto/refs/heads/main/.lua"))()
+    task.wait(2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfx-qudsa/newauto/refs/heads/main/.lua"))()
+]])
 
 local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
 local ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))()
 local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
-
 local Options = Library.Options
 local Toggles = Library.Toggles
-
 local Window = Library:CreateWindow({
     Title = "Crazy Diamond",
     Footer = "script by t.me/IcrazyDiamondI",
@@ -26,30 +29,23 @@ local Window = Library:CreateWindow({
     ShowCustomCursor = false,
     Size = UDim2.new(0, 600, 0, 500),
 })
-
 Window:SetCornerRadius(20)
-
 local Tabs = {
     Settings = Window:AddTab("UI Settings", "settings"),
     Farm = Window:AddTab("autofarm", "hand-coins"),
 }
-
 local MenuGroup = Tabs.Settings:AddLeftGroupbox("Menu", "wrench")
-
 MenuGroup:AddToggle('KeybindList', {
     Text = 'Show Keybind List',
     Default = false,
 })
-
 Toggles.KeybindList:OnChanged(function()
     Library.KeybindFrame.Visible = Toggles.KeybindList.Value
 end)
-
 MenuGroup:AddButton("Copy Telegram", function()
     setclipboard("t.me/IcrazyDiamondI")
     Library:Notify("Telegram copied to clipboard!", 3)
 end)
-
 MenuGroup:AddSlider("UICornerSlider", {
     Text = "Corner Radius",
     Default = 20,
@@ -60,7 +56,6 @@ MenuGroup:AddSlider("UICornerSlider", {
         Window:SetCornerRadius(value)
     end
 })
-
 MenuGroup:AddDropdown("DPIDropdown", {
     Values = { "50%", "75%", "100%", "125%", "150%", "175%", "200%" },
     Default = "100%",
@@ -71,37 +66,29 @@ MenuGroup:AddDropdown("DPIDropdown", {
         Library:SetDPIScale(DPI)
     end,
 })
-
 MenuGroup:AddDivider()
-
 MenuGroup:AddLabel("Menu bind"):AddKeyPicker("MenuKey", { Default = "RightShift", NoUI = true })
 Library.ToggleKeybind = Options.MenuKey
-
 local function Cleanup()
     Library:Unload()
 end
-
 MenuGroup:AddButton("Unload", Cleanup)
 local FarmGroup = Tabs.Farm:AddLeftGroupbox("Autofarm", "play")
-
 FarmGroup:AddButton("Start Autofarm", function()
     _G.AutofarmEnabled = true
     Library:Notify("Autofarm started!", 2)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/vfx-qudsa/newauto/refs/heads/main/.lua"))()
 end)
-
 ThemeManager:SetLibrary(Library)
 SaveManager:SetLibrary(Library)
 SaveManager:IgnoreThemeSettings()
 SaveManager:SetIgnoreIndexes({ "MenuKey" }) 
-
 ThemeManager:SetFolder("ObsidianSoft")
 SaveManager:SetFolder("ObsidianSoft/main")
 SaveManager:BuildConfigSection(Tabs.Settings)
 ThemeManager:ApplyToTab(Tabs.Settings)
 ThemeManager:ApplyTheme("farm.theme")
 SaveManager:LoadAutoloadConfig()
-
 if script then
     script.Destroying:Connect(Cleanup)
 end
