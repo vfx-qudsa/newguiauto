2843267eb8b8ff8a22f94fc3fc4af2008ec3be92
anonimnianonist <vanjamba0211@gmail.com>
2026-08-09 00:28:06 +0300

    Update .lua

--- .lua
+++ .lua
@@ -91,8 +91,8 @@ FarmGroup:AddButton({
     Func = function()
         _G.AutofarmEnabled = true
         Library:Notify("Autofarm started!", 2)
-        queue_on_teleport("_G.AutofarmEnabled = true; loadstring(game:HttpGet('https://raw.githubusercontent.com/vfx-qудса/newguiauto/refs/heads/main/.lua'))()")
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/vfx-qудса/newauto/refs/heads/main/.lua"))()
+        queue_on_teleport("_G.AutofarmEnabled = true; loadstring(game:HttpGet('https://raw.githubusercontent.com/vfx-qudsa/newguiauto/refs/heads/main/.lua'))()")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vfx-qudsa/newauto/refs/heads/main/.lua"))()
     end
 })
 
